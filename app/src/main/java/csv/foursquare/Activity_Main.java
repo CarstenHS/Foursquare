065fcb8b1aac6238fb4aad9633e3ebe62ee1de5c
Carsten <carsers@hotmail.com>
2016-01-29 18:35:41 +0100

    * Updated layout/activity__main.xml * MVP skeleton

--- app/src/main/java/csv/foursquare/Activity_Main.java
+++ app/src/main/java/csv/foursquare/Activity_Main.java
@@ -2,14 +2,38 @@ package csv.foursquare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
-public class Activity_Main extends AppCompatActivity
+public class Activity_Main extends AppCompatActivity implements I_View
 {
+    Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity__main);
+        presenter = new Presenter(this);
+
+        EditText etSearch = (EditText) findViewById(R.id.etSearchString);
+
+        etSearch.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                presenter.searchStringChanged(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
+
+
 }
